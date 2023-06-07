8ae19bcf1bdb77e779dbdafdbc2b7f4c5c865ea5
JosePu1201 <joseluis-pusam@cunoc.edu.gt>
2023-06-07 11:38:42 -0600

    Inicio de proyecto con paquetes graficos creados

---
+++ src/main/java/com/mycompany/compiladorlenguajeespaneol/CompiladorLenguajeEspaneol.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.compiladorlenguajeespaneol;
+
+/**
+ *
+ * @author jose
+ */
+public class CompiladorLenguajeEspaneol {
+
+    public static void main(String[] args) {
+       
+    }
+}
